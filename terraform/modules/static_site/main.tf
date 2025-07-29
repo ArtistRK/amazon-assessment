74955a9a96e469c64541359e8ada801589f2ff54
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-07-29 16:44:21 +0000

    fix

--- terraform/modules/static_site/main.tf
+++ terraform/modules/static_site/main.tf
@@ -35,9 +35,9 @@ data "aws_iam_policy_document" "public" {
 }
 
 resource "aws_s3_bucket" "logs" {
-  bucket         = "${var.bucket_name}-logs"
-  force_destroy  = true
-  tags           = var.tags
+  bucket        = "${var.bucket_name}-logs"
+  force_destroy = true
+  tags          = var.tags
 }
 
 resource "aws_s3_bucket_ownership_controls" "logs" {
@@ -48,39 +48,31 @@ resource "aws_s3_bucket_ownership_controls" "logs" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket = aws_s3_bucket.logs.id
+data "aws_caller_identity" "current" {}
 
-  block_public_acls   = true
-  ignore_public_acls  = true
-  block_public_policy = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_policy" "log_bucket_policy" {
+resource "aws_s3_bucket_policy" "logs_policy" {
   bucket = aws_s3_bucket.logs.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Sid = "AllowCloudFrontServicePrincipal",
-        Effect = "Allow",
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
+        Sid: "AllowCloudFrontLogging",
+        Effect: "Allow",
+        Principal: {
+          Service: "cloudfront.amazonaws.com"
         },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.logs.arn}/*",
-        Condition = {
-          StringEquals = {
-            "AWS:SourceAccount" = "017820696611"
+        Action: "s3:PutObject",
+        Resource: "${aws_s3_bucket.logs.arn}/*",
+        Condition: {
+          StringEquals: {
+            "AWS:SourceAccount": data.aws_caller_identity.current.account_id
           }
         }
       }
     ]
   })
 }
-
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
