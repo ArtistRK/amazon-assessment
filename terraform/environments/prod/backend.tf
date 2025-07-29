1328b821063b8a094a8ad7a50b13271a9bcfb4c9
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-07-29 19:04:28 -0400

    Update backend.tf

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+    backend "s3" {
+        bucket = "fsl-terraform-state-bucket-prod-demo"
+        key = "prod/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform-locks"
+        encrypt = true
+    }
+}
