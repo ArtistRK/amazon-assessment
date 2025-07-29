b680da6f4ff8511c81e15fc08bab9f935347f324
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-07-29 19:05:10 -0400

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,1 +1,13 @@
-ok
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "static_site" {
+  source      = "../../modules/static_site"
+  bucket_name = "fsl-deploy-develop-prod-bucket"
+  build_dir   = "../../build"
+  tags = {
+    Environment = "prod"
+    Project     = "FSL"
+  }
+}
