d5eb6b0862d270d37cf773030a94f214f19ffcc2
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-07-29 19:41:06 -0400

    Update backend.tf

--- terraform/environments/stage/backend.tf
+++ terraform/environments/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "fsl-terraform-state-bucket-stage-demo"
+        bucket = "fsl-terraform-state-bucket-demo"
         key = "stage/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "terraform-locks"
